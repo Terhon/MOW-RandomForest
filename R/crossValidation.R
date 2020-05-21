8507cfac56fc4b597dce6504ef844fa58fd0aa32
Terhon <terhon95@gmail.com>
2020-05-21 17:13:52 +0200

    added k-fold cross validation

---
+++ R/crossValidation.R
@@ -0,0 +1,17 @@
+crossValidation <- function (k, formula, attr, data, numberOfTrees, bootstrap) {
+  size <- nrow(data)/k
+  subsets <- list()
+  for(i in 1:(k-1)) {
+    sample <- sample(seq_len(nrow(data)),size)
+    subsets[[i]] <- data[sample,]
+    data <- data[-sample,]
+  }
+  subsets[[k]] <- data
+  results <- foreach(i=1:k, .combine = c, .multicombine = TRUE, .packages = c(loadedNamespaces())) %dopar% {
+    trainSet <- bind_rows(subsets[(1:k)[-i]])
+    forest <- MOWRandomForest::randomForest(formula, trainSet, numberOfTrees = numberOfTrees, bootstrap = bootstrap, method="anova")
+    prediction <- predict(forest, subsets[[i]], "vector")
+    RMSE(prediction, subsets[[i]][[attr]])
+  }
+  mean(results)
+}
