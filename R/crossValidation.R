59beb155a096908245365946aa6695c517de924e
Terhon <terhon95@gmail.com>
2020-05-24 20:54:58 +0200

    moving functions and updating testing script

--- R/crossValidation.R
+++ R/crossValidation.R
@@ -36,3 +36,47 @@ crossValidation <- function(k, formula, targetAttr, data, numberOfAttributes, nu
 
   collect(results)
 }
+
+getConfusionMatrixMetricsClass <- function(prediction, real){
+  real <- factor(real, levels=levels(real))
+  prediction <- factor(prediction, levels=levels(prediction))
+  
+  confMatrix <- caret::confusionMatrix(prediction, real, mode="prec_recall")
+  overall <- confMatrix$overall
+  classInfo <- confMatrix$byClass
+  
+  accuracyMetric <- overall[['Accuracy']]
+  precisionMetric <- classInfo[,'Precision']
+  recallMetric <- classInfo[,'Recall']
+  
+  list(list(accuracy = accuracyMetric, precision = precisionMetric, recall = recallMetric))
+}
+
+collectCrossValidationClass <- function(crossMetricsList){
+  accuracy <- crossMetricsList[[1]]$accuracy
+  precision <- crossMetricsList[[1]]$precision
+  recall <- crossMetricsList[[1]]$recall
+  
+  accuracy[is.na(accuracy)] <- 0
+  precision[is.na(precision)] <- 1
+  recall[is.na(recall)] <- 1
+  
+  for (i in 2:length(crossMetricsList)){
+    metrics <- crossMetricsList[[i]]
+    
+    metrics$accuracy[is.na(metrics$accuracy)] <- 0
+    metrics$precision[is.na(metrics$precision)] <- 1
+    metrics$recall[is.na(metrics$recall)] <- 1
+    
+    accuracy <- accuracy + metrics$accuracy
+    precision <- precision + metrics$precision
+    recall <- recall + metrics$recall
+  }
+  
+  n <- length(crossMetricsList)
+  
+  accuracy <- accuracy / n
+  precision <- precision / n
+  recall <- recall / n
+  list(list(accuracy = accuracy, precision = precision, recall = recall))
+}
